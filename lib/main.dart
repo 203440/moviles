4e50bed87cf88c78d3bb5e048f37a8b5cac973ad
maricruz ruiz <karlamaricruz2904@gmail.com>
2023-02-01 16:56:39 -0600

    file registration and login

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/login.dart';
+import 'package:flutter_application_1/screens/onboarding/body_boarding.dart';
+//import 'package:flutter_application_1/pages/register.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const BodyBoarding(),
+    );
+  }
+}
---
+++ lib/screens/onboarding/components/content_boarding.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ContentBoarding extends StatelessWidget {
+  const ContentBoarding({super.key, this.title, this.title2, this.image});
+  final String? title, title2, image;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(title!),
+        Image(
+          image: AssetImage(image!),
+        ),
+        Text(title2!),
+      ],
+    );
+  }
+}
